d4ef183ab7560715f94efbfb274d35fd6cddc462
Dimplegurajala <dimplegurajala@gmail.com>
2026-03-28 11:09:14 +0530

    CI: Day 4-radio Button-

--- Week2/Day4/HW4-radio.spec.ts
+++ Week2/Day4/HW4-radio.spec.ts
@@ -3,6 +3,7 @@ test("Radio Buttons", async({page})=>{
     await page.goto("https://leafground.com/radio.xhtml")
     const defaultBrowser= await page.locator('//label[contains(text(),"Safari")]')
     //Identify and assert the default selected radio button.
+//problem with finding the default -- trying to solve//
     // const default = await page.locator
     await page.click('//label[text()="Chrome"][1]')
     const mostfavBrowser = page.locator('//label[text()="Chrome"][1]')
