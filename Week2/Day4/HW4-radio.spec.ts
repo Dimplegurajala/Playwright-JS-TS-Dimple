c485319e63e2d386405b2ed6632348989e303288
Dimplegurajala <dimplegurajala@gmail.com>
2026-03-28 11:29:56 +0530

    CI:Day4-radiobutton

--- Week2/Day4/HW4-radio.spec.ts
+++ Week2/Day4/HW4-radio.spec.ts
@@ -1,15 +1,15 @@
 import {test, expect} from '@playwright/test'
 test("Radio Buttons", async({page})=>{
     await page.goto("https://leafground.com/radio.xhtml")
-    const defaultBrowser= await page.locator('//label[contains(text(),"Safari")]')
+    //const defaultBrowser= await page.locator('//label[contains(text(),"Safari")]')
     //Identify and assert the default selected radio button.
 //problem with finding the default -- trying to solve//
     // const default = await page.locator
     await page.click('//label[text()="Chrome"][1]')
-    const mostfavBrowser = page.locator('//label[text()="Chrome"][1]')
-    await expect(mostfavBrowser).toBeChecked()
+    //const mostfavBrowser = await page.locator('//td)]).innerText()
+    //await expect(mostfavBrowser).toContainText('Chrome')
     await page.click('//label[text()="Bengaluru"]/parent::div')
-    await page.click('//label[contains(text(),"21-40 Years")]/parent::div')
-    const ageGroup = page.locator('//label[contains(text(),"21-40 Years")]/parent::div')
+    //await page.click('//label[contains(text(),"21-40 Years")]/preceding-sibling::div')
+    const ageGroup = page.locator('//label[contains(text(),"21-40 Years")]/preceding-sibling::div//input')
     await expect(ageGroup).toBeChecked()
 })
