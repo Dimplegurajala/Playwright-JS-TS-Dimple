2e91c8d26c54f73e8117a0ad07099fc4958c3067
Dimplegurajala <dimplegurajala@gmail.com>
2026-04-02 01:11:54 +0530

    Remove commented expectation for message content

--- Week3/Day2/HW2-Playwright.spec.ts
+++ Week3/Day2/HW2-Playwright.spec.ts
@@ -9,7 +9,6 @@ test("Auto-Waiting", async({page})=>{
     await page.getByText('Click').nth(1).click()
     await expect(page.getByText('I am about to hide')).toBeHidden({ timeout: 15000})
     await page.getByText('Click First Button').click()
-    //await expect(page.getByText('Message Content')).toBeHidden({ timeout: 15000})
     await expect(page.getByText('Click Second')).toBeEnabled({ timeout: 15000})
     await expect(page.getByText('I am going to change!')).toBeVisible()
     await page.getByText('Click').last().click()
