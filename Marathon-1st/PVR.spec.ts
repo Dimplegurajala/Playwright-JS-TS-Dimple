2b83b4ca6c96179d5e486edc2957ef37f535256a
Dimplegurajala <dimplegurajala@gmail.com>
2026-04-04 00:55:08 +0530

    CI:PVR assesment

--- Marathon-1st/PVR.spec.ts
+++ Marathon-1st/PVR.spec.ts
@@ -3,11 +3,32 @@ import {test, expect} from '@playwright/test'
 test("Verify dynamic movie ticket booking flow in PVR Cinemas website", async({page})=>{
     await page.goto("https://www.pvrcinemas.com/")
     await page.getByRole("heading",{name:"Bengaluru"} ).click()
-    await page.getByText("Cinema").click()
-    await page.getByRole('button', {name:"Select Cinema"}).click()
+    await page.locator('//span[text()="Cinema"]').click()
+    await page.locator('//div[@id="cinema"]//span[text()="Select Cinema"]').click()
     await page.getByText("INOX Nexus Whitefield").click()
-    //await page.getByText("Select Date").click()
     await page.getByText(/Tomorrow/i).click()
-    
+    //await page.locator('//div[@id="movie"]').click()
+    await page.locator('//li[@class="p-dropdown-item"]').first().click()
+    //await page.getByText("Select Timing").first().click()
+    await page.locator('(//ul[@role="listbox"]//span/span[@class="mx-2"])[1]').click()
+    await page.getByRole('button', {name:"Submit"}).click()
+    await page.getByRole('button', {name:"Accept"}).click()
+    const primeSeat = page.locator('span[id*="PRIME ROWS"].seat-current-pvr').first()
+    const seatId = await primeSeat.getAttribute('id')
+    const clickedNumber = seatId!.split('|')[1].replace(':', '')
+    //.replace(/[^/w]+/g,'')
+    await primeSeat.click()
+    const SeatInfo = page.locator('//div[@class= "seat-number"]/p')//.innerText()
+    await expect(SeatInfo).toContainText(clickedNumber)
+    // verify if total is displayed
+    await expect(page.getByText("Grand-Total")).toBeVisible
+    const pageTitle = await page.title()
+    console.log(`The page title is ${pageTitle}`)
+    //proceed
+    await page.getByRole('button',{name:"Proceed"}).click()
+
+
+
+
 
 })
